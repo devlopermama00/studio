0527a82346960d6251e723b761e61a3f08365321
Eng <youngseagulmama@gmail.com>
2025-07-06 21:23:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/paypal.ts
+++ src/lib/paypal.ts
@@ -4,7 +4,7 @@
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = process.env;
 const base = "https://api-m.sandbox.paypal.com";
 
-async function getPayPalAccessToken() {
+export async function getPayPalAccessToken() {
     const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`).toString('base64');
     const response = await fetch(`${base}/v1/oauth2/token`, {
         method: 'POST',
