dcc7fc2c974ea5e50ffa2830adbd2b25884a3850
Eng <youngseagulmama@gmail.com>
2025-07-08 09:39:04 +0000

    when admin is uploading the favicon its not get updated.

--- src/lib/settings-data.ts
+++ src/lib/settings-data.ts
@@ -2,8 +2,10 @@
 'use server';
 import dbConnect from '@/lib/db';
 import Setting from '@/models/Setting';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function getSettings() {
+    noStore();
     await dbConnect();
     try {
         const settingsArray = await Setting.find({}).lean();
